9071fffbc2858e2b0b36f219864fed8739c3d111
HoaNguyen <hoanguyenquoc2002@gmail.com>
2022-11-21 15:45:05 +0700

    library update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(['youTube', 'facebook', 'yalo']);
 
-  const handleIncrease = () => {
-      setCounter([...counter, 'tiktok'])
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+
+  const handleName = e => setName(e.target.value);
+  const handleEmail = e => setEmail(e.target.value)
+  const handlSubit = () => {
+    console.log({
+      name,
+      email
+    })
   }
 
-  return (
-    <div className="App">
-      <h1>{JSON.stringify(counter)}</h1>
+  return(
+    <div style={{ padding: 50 }}>
+      <input
+        value={ name }
+        onChange={handleName}
+      /> <br/>
+      <input
+        value={email}
+        onChange={handleEmail}
+      />
 
-      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handlSubit}>submit</button>
     </div>
-  );
+  )
 }
 
 export default App;
